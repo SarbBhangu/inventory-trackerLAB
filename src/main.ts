cdbd16520f56c7e4c90a66f1eafbf5ac2a457578
SarbBhangu <Bhangu513@gmail.com>
2025-11-12 16:16:34 -0500

    Use a loop to display the details of each product

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
+import { PhysicalProduct } from "./models/PhysicalProduct.js";
+import { DigitalProduct } from "./models/DigitalProduct.js";
+
+const phone = new PhysicalProduct("A0316", "Phone", 1000, 0.4);
+const dumbbell = new PhysicalProduct("A0317", "Dumbbell", 49, 10);
+const ebook = new DigitalProduct("A0318", "Fitness Ebook", 29, 20);
+
+const inventory = [phone, dumbbell, ebook];
+
+for (const item of inventory) {
+    console.log(item.displayDetails());
+    console.log("Final price:", item.getPriceWithTax());
+}
 
 
 
