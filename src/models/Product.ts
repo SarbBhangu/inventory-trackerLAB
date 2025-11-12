e7d5c4721d27063bc3c16ec720b9958fc9bb7686
SarbBhangu <Bhangu513@gmail.com>
2025-11-12 15:06:45 -0500

    added product base class

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,2 +1,21 @@
+export class Product {
+    sku: string;
+    name: string;
+    price: number;
+
+    constructor(sku:string, name: string, price: number) {
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+    }
+
+    displayDetails(): string {
+        return `SKU: ${this.sku} | Name: ${this.name} | Price: ${this.price}`;
+    }
+
+    getPriceWithTax(): number {
+        return this.price;
+    }
+}
 
 
